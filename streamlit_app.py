6419e47f4690af8321877db1c9c977cf7060a0b3
MorganTBounds <87327714+MorganTBounds@users.noreply.github.com>
2022-02-06 16:15:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,25 +5,19 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# MY APP
 
-TEST TEST TEST TEST
+This is my app. Yay! 
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+## FAQ
+1. How does it work? 
 """
 
 
 with st.echo(code_location='below'):
     
     txt = st.text_area('Copy and Paste Tweet Below:', '''
-     It was the best of times, it was the worst of times, it was
-     the age of wisdom, it was the age of foolishness, it was
-     the epoch of belief, it was the epoch of incredulity, it
-     was the season of Light, it was the season of Darkness, it
-     was the spring of hope, it was the winter of despair, (...)
+     Lorem ipsum lorem ipsum lorem ipsum... 
      ''', max_chars=280)
     
     st.write('Sentiment:', txt)

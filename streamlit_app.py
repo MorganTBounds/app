93c0f380ca9809b249b0a401a709f5cb58b1ae78
MorganTBounds <87327714+MorganTBounds@users.noreply.github.com>
2022-02-06 18:29:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,12 +12,13 @@ This is my app. Yay!
 1. How does it work? 
 """
 
-credential_container = st.empty()
+key_container = st.empty()
+token_container = st.empty()
 
 public_key = '194DYG3yLlLALXtzL4XHYgLyV'
 public_token = '1421108778842349570-4OM14pkDa47PXsP7TzSHMUfHqYQWjV'
-private_key = credential_container.text_input('Enter Private Key:', '', type='password')
-private_token = credential_container.text_input('Enter Private Access Toekn:', '', type='password')
+private_key = key_container.text_input('Enter Private Key:', '', type='password')
+private_token = token_container.text_input('Enter Private Access Toekn:', '', type='password')
 
 # fire up the Twitter API using Tweepy 
 auth = tweepy.OAuthHandler(public_key, private_key)
@@ -29,7 +30,8 @@ if st.button('Submit'):
      try:
           api.verify_credentials()
           st.markdown("Credentials successfully verified!")
-          credential_container.empty()
+          key_container.empty()
+          token_container.empty()
 
      except:
           st.markdown("Bad credentials... Please try again!")  

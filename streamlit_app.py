14f81a1e3a5b840e8fe11589d3da03913c20ea2b
MorganTBounds <87327714+MorganTBounds@users.noreply.github.com>
2022-02-06 21:34:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,26 +11,48 @@ This is my app. Yay!
 ## FAQ
 1. How does it work? 
 """
-
-with st.form("credentials"):
+if 'is_credential' not in st.session_state:
+     st.session_state.is_credential = False
+     
+if 'private_key' not in st.session_state:
+     st.session_state.private_key = ''
+     
+if 'private_token' not in st.session_state:
+     st.session_state.private_token = ''
+     
+with st.form:
      public_key = '194DYG3yLlLALXtzL4XHYgLyV'
      public_token = '1421108778842349570-4OM14pkDa47PXsP7TzSHMUfHqYQWjV'
-     private_key = st.text_input('Enter Private Key:', '', type='password')
-     private_token = st.text_input('Enter Private Access Token:', '', type='password')
+     
+     key_container = st.empty()
+     token_container = st.empty()
+     
+     if st.session_state.private_key == '':
+          private_key = key_container.text_input('Enter Private Key:', '', type='password')
+          private_token = token_container.text_input('Enter Private Access Token:', '', type='password')
 
-     # fire up the Twitter API using Tweepy 
-     auth = tweepy.OAuthHandler(public_key, private_key)
-     auth.set_access_token(public_token, private_token)
-     api = tweepy.API(auth, wait_on_rate_limit=True)
+          # Every form must have a submit button.
+          if st.form_submit_button("Submit"):
+               try:
+                    # fire up the Twitter API using Tweepy 
+                    auth = tweepy.OAuthHandler(public_key, private_key)
+                    auth.set_access_token(public_token, private_token)
+                    api = tweepy.API(auth, wait_on_rate_limit=True)
+                    api.verify_credentials()
 
-     # Every form must have a submit button.
-     if st.form_submit_button("Submit"):
-          try:
-               api.verify_credentials()
-               st.markdown("Credentials successfully verified!")
+                    key_container.empty()
+                    token_container.empty()
+                    st.session_state.private_key = private_key
+                    st.session_state.private_token= private_token         
+                    st.markdown("Credentials successfully verified!")
 
-          except:
-               st.markdown("Bad credentials... Please try again!")  
+               except:
+                    st.markdown("Bad credentials... Please try again!")  
+     else:
+          auth = tweepy.OAuthHandler(public_key, private_key)
+          auth.set_access_token(public_token, private_token)
+          api = tweepy.API(auth, wait_on_rate_limit=True)
+          st.markdown("Credentials successfully verified!")
                
 
 

2b834f6688f5539b3928240449f3591bf13f617d
MorganTBounds <87327714+MorganTBounds@users.noreply.github.com>
2022-02-06 19:24:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,25 @@ This is my app. Yay!
 ## FAQ
 1. How does it work? 
 """
+with st.form("credentials"):
+     
+     public_key = '194DYG3yLlLALXtzL4XHYgLyV'
+     public_token = '1421108778842349570-4OM14pkDa47PXsP7TzSHMUfHqYQWjV'
+     private_key = st.text_input('Enter Private Key:', '', type='password')
+     private_token = st.text_input('Enter Private Access Token:', '', type='password')
+     
+     # fire up the Twitter API using Tweepy 
+     auth = tweepy.OAuthHandler(public_key, private_key)
+     auth.set_access_token(public_token, private_token)
+     api = tweepy.API(auth, wait_on_rate_limit=True)
 
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Submit")
+     
+    if submitted:
+        st.write("slider", slider_val, "checkbox", checkbox_val)
+          
+"""
 credential_container = st.empty()
 url_container = st.empty()
 
@@ -39,7 +57,9 @@ with credential_container.container():
                st.markdown("Bad credentials... Please try again!")  
                
           
-          
+    
+"""
+
 """
 
 url = 'https://twitter.com/KimKardashian/status/1489401564284346369?s=20&t=nMf-OpIe73e8Gvnh--9kPA'

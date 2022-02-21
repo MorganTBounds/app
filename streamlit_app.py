3ddb6a741b8a1ae2cff54ae66b8000dda0668683
MorganTBounds <87327714+MorganTBounds@users.noreply.github.com>
2022-02-20 19:11:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import tweepy
 import pandas as pd
+import pickle
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer

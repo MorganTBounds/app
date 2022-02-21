a2a0b80248141ed798df3655f80f0fdfbc1ca3f6
MorganTBounds <87327714+MorganTBounds@users.noreply.github.com>
2022-02-20 19:26:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -100,6 +100,6 @@ if st.session_state.is_credential:
      st.session_state.tweet_text = text
 
      if st.button('Classify Tweet'):
-          pred = clf.predict_proba([st.session_state.tweet_text])
+          pred = pd.DataFrame(clf.predict_proba([st.session_state.tweet_text]), columns=['Not Hateful', 'Hateful'])
           st.barchart(pred)
     
